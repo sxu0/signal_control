22cd22ec9cb53f090f4bd3813616416867b04813
sxu0 <sxu0726@gmail.com>
2023-01-12 15:31:16 -0500

    fix indexing bug

--- uncertainty_test.py
+++ uncertainty_test.py
@@ -9,7 +9,6 @@ LOOKUP = 6  # {183 GHz band, dual pol, either fft} test tones
 
 df = pd.read_csv('test_tones.csv', header=None, skiprows=3)
 test_tones = df.iloc[:,LOOKUP] / 1000  # convert MHz to GHz
-test_tones = test_tones[75:]
 
 power_levels = [0, 5, 10, 15]  # with 80 dBm of attenuators
 # power_levels = [0, 5, 10]  # with 60 dBm of attenuators
@@ -26,7 +25,7 @@ synthesizer.write('OUTP:STAT ON')  # power on
 for j in range(len(power_levels)):
     synthesizer.write('POW:LEV %f DBM' %(power_levels[j]))  # set power level
     time.sleep(2)
-    for i in range(len(test_tones)):
+    for i in range(75, len(test_tones)):
         print(i)
         synthesizer.write('FREQ:CW %f GHZ' %(test_tones[i]))  # set freq
         time.sleep(30 * int_time)
